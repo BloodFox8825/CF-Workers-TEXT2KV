cf56caed279e65a221971807583440edb952bde2
BloodFox8825 <127063466+BloodFox8825@users.noreply.github.com>
2024-05-17 15:03:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -89,36 +89,8 @@ export default {
 
 			
 		} else if (url.pathname == "/"){//首页改成一个nginx伪装页
-			return new Response(`
-			<!DOCTYPE html>
-			<html>
-			<head>
-			<title>Welcome to nginx!</title>
-			<style>
-				body {
-					width: 35em;
-					margin: 0 auto;
-					font-family: Tahoma, Verdana, Arial, sans-serif;
-				}
-			</style>
-			</head>
-			<body>
-			<h1>Welcome to nginx!</h1>
-			<p>If you see this page, the nginx web server is successfully installed and
-			working. Further configuration is required.</p>
-			
-			<p>For online documentation and support please refer to
-			<a href="http://nginx.org/">nginx.org</a>.<br/>
-			Commercial support is available at
-			<a href="http://nginx.com/">nginx.com</a>.</p>
-			
-			<p><em>Thank you for using nginx.</em></p>
-			</body>
-			</html>
-			`, {
-			  headers: {
-				'Content-Type': 'text/html; charset=UTF-8',
-			  },
+			return new Response(``, {
+			  status: 404,
 			});
 		} else {// 如果 token 不符，返回 'token 有误'//
 			return new Response('token 有误', {
